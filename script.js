3196c6f966d83ad4874ab7533d9997f45d31af64
GURUBALAN . V <121040854+Teejayguru@users.noreply.github.com>
2025-04-29 10:41:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// navbar start
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+// when we click on hamburger icon its close 
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+// Navbar end
+
+
+// Skills 
+
+
+
+
+
+// Skills 
+
+
